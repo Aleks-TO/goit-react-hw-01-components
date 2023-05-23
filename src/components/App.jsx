0ce8219b9b46b94dd690867d381244207c3d439a
Aleks-TO <satov69@gmail.com>
2023-05-23 18:14:04 +0300

    04-task transactions complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import { Statistics } from './02-statistics/Statistics';
 import data from './02-statistics/data.json';
 import FriendList from './03-friendList/FriendList';
 import friends from './03-friendList/friends.json';
+import TransactionHistory from './04-transactionHistory/TransactionHistory';
+import transactions from './04-transactionHistory/transactions.json';
 
 export const App = () => {
   return (
@@ -18,6 +20,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
     </>
   );
 };
